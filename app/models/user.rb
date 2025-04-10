6a47248d7a7088237cd77b81ee6c054587126660
Arique <arique1104@gmail.com>
2025-04-10 09:59:42 -0700

    Adds database relationships for User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
+# app/models/user.rb
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :pies_entries
+  has_many :team_assignments, foreign_key: :leader_id, class_name: 'TeamAssignment'
+  has_many :assigned_users, through: :team_assignments, source: :individual
+
+  validates :role, inclusion: { in: %w[individual leader owner] }
 end

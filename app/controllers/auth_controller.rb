dca8347118b62f09df99b67134c7b0d2173ff240
Arique <arique1104@gmail.com>
2025-04-12 10:22:22 -0700

    Updates auth controller to allow for secure sign ups depending on role

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,12 +2,17 @@ class AuthController < ApplicationController
     skip_before_action :authorize_request, only: [ :login, :signup ]
 
     def signup
-      user = User.new(user_params.merge(role: :individual))
+      requested_role = params[:role]
+      allowed_roles = User.roles.keys
+      role = allowed_roles.include?(requested_role) ? requested_role : 'individual'
+
+      user = User.new(user_params.merge(role: role))
       if user.save
         token = JsonWebToken.encode(user_id: user.id)
         render json: { token: token, user: user }, status: :created
       else
-        render json: { erros: user.errors.full_messages }, status: :unprocessable_entity
+        puts "Signup error: #{user.errors.full_messages}"
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
       end
     end
 

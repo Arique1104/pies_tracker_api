c553b217d8549efdddcf7eee63a7628fb27ea09e
Arique <arique1104@gmail.com>
2025-04-17 10:19:53 -0700

    Adds reset_password_token and sent_at columns to the users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_16_195744) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_17_150449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -70,6 +70,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_16_195744) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
   end
 
   add_foreign_key "pies_entries", "users"

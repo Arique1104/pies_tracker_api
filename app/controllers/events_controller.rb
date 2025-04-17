2151f34cc59c0c0e66b744451ea9954379882b33
Arique <arique1104@gmail.com>
2025-04-17 13:22:35 -0700

    Adds events controller and updated routes

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,45 @@
+class EventsController < ApplicationController
+  before_action :authorize_request
+
+  def index
+    render json: Event.includes(:hosts).order(date: :asc), status: :ok
+  end
+
+  def create
+    event = Event.new(event_params.merge(created_by_id: @current_user.id))
+
+    if event.save
+      # Add all provided host_ids
+      event.hosts << User.where(id: params[:host_ids]) if params[:host_ids]
+      render json: event, status: :created
+    else
+      render json: { errors: event.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    event = Event.find(params[:id])
+
+    unless event.hosts.include?(@current_user)
+      return render json: { error: "Unauthorized" }, status: :unauthorized
+    end
+
+    if event.update(event_params)
+      event.hosts = User.where(id: params[:host_ids]) if params[:host_ids]
+      render json: event, status: :ok
+    else
+      render json: { errors: event.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    event = Event.includes(:hosts).find(params[:id])
+    render json: event, status: :ok
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date, :location)
+  end
+end

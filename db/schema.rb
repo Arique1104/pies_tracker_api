9669bde906f643e41879cc1f5804a8963fec10e5
Arique <arique1104@gmail.com>
2025-04-17 17:47:28 -0700

    Creates Organization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_17_201113) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_18_004322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -36,6 +36,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_17_201113) do
     t.datetime "date"
     t.string "location"
     t.integer "created_by_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
